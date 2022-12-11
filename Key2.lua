e4167f3e8b83f07ec5e71a76cabdd88e2d3c8957
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-12-11 17:09:19 +0000

    Update Key2.lua

--- Key2.lua
+++ Key2.lua
@@ -1,35 +1,31 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
-
-
-
 📋
 local Window = Rayfield:CreateWindow({
    Name = "ReeqyHub",
-   LoadingTitle = "ReeqyHub",
-   LoadingSubtitle = "ReeqyHub",
+   LoadingTitle = "Testing",
+   LoadingSubtitle = "Reeqy",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = true, -- Create a custom folder for your hub/game
-      FileName = "ReeqyHub"
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
    },
    Discord = {
-      Enabled = True,
-      Invite = "https://discord.gg/s9C9dWhAqV", -- The Discord invite code, do not include discord.gg/
+      Enabled = false,
+      Invite = "Reeqy", -- The Discord invite code, do not include discord.gg/
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "ReeqyHub",
+      Title = "Reeqy",
       Subtitle = "Key System",
-      Note = "Join the discord https://discord.gg/s9C9dWhAqV)",
-      FileName = "ReeqyHubKey",
+      Note = "Your a big nub",
+      FileName = "KeyReeqy",
       SaveKey = true,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = "Sophie1"
-  
-      
-      local Tab = Window:CreateTab("Testing", 4483362458)
-
-    }
+   }
 })
 
+local Tab = Window:CreateTab("ReeqyHub", 4483362458) -- Title, Image
+
+
+local Section = Tab:CreateSection("Testingyourmum")
